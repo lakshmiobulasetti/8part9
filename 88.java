2b559a5705e7a70085eb99d5a0a3c2eb742dac86
lakshmiobulasetti <41731019+lakshmiobulasetti@users.noreply.github.com>
2018-09-08 12:15:04 +0530

    Update and rename README.md to 88.java

---
+++ 88.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	int n1 = 72, n2 = 120, lcm;
+	lcm = (n1 > n2) ? n1 : n2;
+	while(true)
+            {  
+            if( lcm % n1 == 0 && lcm % n2 == 0 )
+            {
+                System.out.printf("The LCM of %d and %d is %d.", n1, n2, lcm);
+                break;
+            }
+            ++lcm;
+        }
+    }
+}
+	
